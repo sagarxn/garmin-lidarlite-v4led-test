573c60fd1d7532d15bf44684644e94125e06613b
Sagar Chaudhary <078bei031.sagar@pcampus.edu.np>
2026-07-14 15:47:16 +0545

    chore: add remaining conifig files

--- main/main.c
+++ main/main.c
@@ -1,11 +1,17 @@
 #include "esp_check.h"
 
 #include "board.h"
+#include "storage.h"
 #include "sensor.h"
+#include "cm.h"
 
 void app_main(void)
 {
     ESP_ERROR_CHECK(board_init());
+
+    ESP_ERROR_CHECK(storage_init());
+    
+    ESP_ERROR_CHECK(cm_init());
 
     ESP_ERROR_CHECK(sensor_init());
 }
